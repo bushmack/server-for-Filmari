aad836334e0ba36b2f1ebfc0f44985b2b08ad1cb
bushmack <krute3684@gmail.com>
2026-02-11 11:41:29 +0300

    main

--- main.py
+++ main.py
@@ -68,3 +68,7 @@ async def api_get_user_collections_endpoint(user_id: str):
         return get_user_collections(user_id)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
